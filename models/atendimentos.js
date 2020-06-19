a1f6ad5e03a41ac81b0ad8b6f7aee8538ce47691
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-06-18 21:57:50 -0300

    Trabalhando com status http nas respostas e aplicando validações de dados com mensagens de erro ao usuário.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,21 +2,46 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
 
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data =  moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
 
-        const sql = 'INSERT INTO atendimentos SET ?'
+        /*VALIDAÇÕES*/
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome:'data',
+                valido:dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome:'cliente',
+                valido:clienteEhValido,
+                mensagem: 'Cliente deve ter no mínimo cinco caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros){
+            res.status(400).json(erros)
+        }else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+
+            const sql = 'INSERT INTO atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
